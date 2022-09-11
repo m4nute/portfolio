54b38e78ae506a048b9495862cf7fd1825b7b6c7
m4nutee <mgudinocontacto@gmail.com>
2022-09-11 18:12:46 -0300

    v1

--- src/components/Navbar2.js
+++ src/components/Navbar2.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
@@ -6,22 +6,25 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import "bootswatch/dist/lux/bootstrap.min.css";
 import "../styles/Navbar.scss";
-AOS.init({
-    duration: 900,
-    offset: 0,
-    once: true
-});
+
 function Navbar2() {
+    const [expanded, setExpanded] = useState(false);
+    AOS.init({
+        duration: 900,
+        offset: 0,
+        once: true
+    });
   return (
-    <Navbar bg="dark" expand="sm" variant='dark' data-aos='fade-down'>
+    <Navbar bg="dark" expand="sm" variant='dark' data-aos='fade-down' fixed='top' expanded={expanded}>
             <Container>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#intro" data-aos='fade-down'>Sobre Mi</Nav.Link>
-                        <Nav.Link href="#skills" data-aos='fade-down' data-aos-delay="100" >Conocimientos</Nav.Link>
-                        <Nav.Link href="#projects" data-aos='fade-down'data-aos-delay="200" >Proyectos</Nav.Link>
-                        <Nav.Link href="#contact" data-aos='fade-down' data-aos-delay="300" >Contactame</Nav.Link>
+                        <Nav.Link onClick={() => setTimeout(() => {setExpanded(false)}, 150)} href="#intro" data-aos='fade-right'><span id='navlink'>Sobre Mi</span></Nav.Link>
+                        <Nav.Link onClick={() => setTimeout(() => {setExpanded(false)}, 150)} href="#skills" data-aos='fade-right' data-aos-delay="100" ><span id='navlink'>Conocimientos</span></Nav.Link>
+                        <Nav.Link onClick={() => setTimeout(() => {setExpanded(false)}, 150)} href="#projects" data-aos='fade-right' data-aos-delay="200" ><span id='navlink'>Proyectos</span></Nav.Link>
+                        <Nav.Link onClick={() => setTimeout(() => {setExpanded(false)}, 150)} href="#contact" data-aos='fade-right' data-aos-delay="300" ><span id='navlink'>Contactame</span></Nav.Link>
+                        <Nav.Link onClick={() => window.open("https://", "_blank")} href="#contact" data-aos='fade-right' data-aos-delay="300" ><span id='navlink'>Contactame</span></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
